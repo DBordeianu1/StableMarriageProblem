5d8d799cce6e90628f09679d3ebe6f24317b7f3d
Daniela Bordeianu <danabordeianu55@gmail.com>
2026-04-19 02:08:40 -0400

    feat: finish basic functions + complete offer and evaluate functions for recursive GS

--- SchemeFunctional/main.scm
+++ SchemeFunctional/main.scm
@@ -10,6 +10,7 @@
 (define (programRol L) (cadddr L))
 
 ; define quota as well
+(define (get-quota L) (caddr L))
 
 ; Find the id of a resident
 (define (resID L) (car L) )
@@ -63,21 +64,32 @@ Returns the rank of a resident in the rol of a program, 0 being the 1st rank
 #|
 Returns true if this program has been matched to a program
 
-These tests were provided by the professor, but not useful if gale-shapley not implemented yet:
+These tests were provided by the Professor, but not useful if gale-shapley not implemented yet:
 > (matched? 403 (gale-shapley RLIST PLIST '()))
 #t
 > (matched? 377 (gale-shapley RLIST PLIST '()))
 #f
 
 So here are some custom tests:
-> (matched? 403 '((574 "NRS") (913 "MMI") (403 "HEP")))
+> (matched? 403 '(("OBG" ((773 . 2) (828 . 1) (616 . 0))) ("MMI" ((226 . 2))) ("HEP"
+((913 . 2) (403 . 0))) ("NRS" ((126 . 5) (517 . 1) (574 . 0)))))
 #t
-> (matched? 377 '((403 "HEP") (574 "NRS") (913 "MMI")))
+> (matched? 377 '(("OBG" ((773 . 2) (828 . 1) (616 . 0))) ("MMI" ((226 . 2))) ("HEP"
+((913 . 2) (403 . 0))) ("NRS" ((126 . 5) (517 . 1) (574 . 0)))))
 #f
 |#
+; Helper
+(define (res-in-pro? rid program-matches)
+  (cond ((null? program-matches) #f)
+        ((eq? rid (caar program-matches)) #t)
+        (else (res-in-pro? rid (cdr program-matches)))
+    )
+  )
+
+; Actual implementation
 (define (matched? rid matches)
 (cond ((null? matches) #f)
-((eq? rid (resID(car matches))) #t)
+((res-in-pro? rid (cadar matches)) #t)
 (else (matched? rid (cdr matches)))
   )
   )
@@ -85,8 +97,126 @@ So here are some custom tests:
 #|
 Provides information of the matchings associated to a program
 
+These tests were provided by the Professor, but not useful if gale-shapley not implemented yet:
 > (get-match "HEP" (gale-shapely RLIST PLIST '()))
 ("HEP" ((913 . 2) (403 . 0)))
 > (get-match "NRS" (gale-shapely RLIST PLIST '()))
 ("NRS" ((126 . 5) (517 . 1) (574 . 0)))
+
+So here are some custom tests:
+> (get-match "HEP" '(("OBG" ((773 . 2) (828 . 1) (616 . 0))) ("MMI" ((226 . 2))) ("HEP" ((913 . 2) (403 . 0))) ("NRS" ((126 . 5) (517 . 1) (574 . 0)))))
+("HEP" ((913 . 2) (403 . 0)))
+> (get-match "NRS" '(("OBG" ((773 . 2) (828 . 1) (616 . 0))) ("MMI" ((226 . 2))) ("HEP" ((913 . 2) (403 . 0))) ("NRS" ((126 . 5) (517 . 1) (574 . 0)))))
+("NRS" ((126 . 5) (517 . 1) (574 . 0)))
 |#
+(define (get-match pid matches)
+(cond ((null? matches) (list pid))
+((string=? pid (caar matches)) (car matches))
+(else (get-match pid (cdr matches)))
+)
+  )
+
+#|
+Add a resident (+ its rank) to the list of residents matched to a program
+
+Test provided by the Professor, but not useful if gale-shapley not implemented yet:
+> (add-resident-to-match (cons 828 3)(get-match "NRS" (gale-shapely RLIST PLIST '())))
+("NRS" ((126 . 5) (828 . 3) (517 . 1) (574 . 0)))
+
+So here is a custom test:
+> (add-resident-to-match (cons 828 3) '("NRS" ((126 . 5) (517 . 1) (574 . 0))))
+("NRS" ((126 . 5) (828 . 3) (517 . 1) (574 . 0)))
+|#
+; Helper
+(define (append-desc-rank pair match-no-car)
+  (cond ((< (cdar match-no-car) (cdr pair)) (cons pair match-no-car))
+        (else (cons (car match-no-car) (append-desc-rank pair (cdr match-no-car))))
+    )
+  )
+
+; Actual implementation
+(define (add-resident-to-match pair match)
+  (cond ((null? match) '())
+        ((null? (cdr match)) (list (car match) (list pair)))
+        (else (list (car match)(append-desc-rank pair (cadr match))))
+    )
+  )
+
+#|
+In order to implement the McVitie-Wilson algorithm, we will need to implement offer
+
+> (offer (car RLIST) RLIST PLIST '())
+(("NRS" ((574 . 0))))
+> (offer (cadr RLIST) RLIST PLIST
+(offer (car RLIST) RLIST PLIST '()))
+(("NRS" ((517 . 1) (574 . 0))))
+> (offer (caddr RLIST) RLIST PLIST
+(offer (cadr RLIST) RLIST PLIST
+(offer (car RLIST) RLIST PLIST '())))
+(("HEP" ((403 . 0))) ("NRS" ((517 . 1) (574 . 0))))
+|#
+(define (offer-helper resRol rinfo rlist plist matches)
+  (cond ((null? resRol) matches)
+        (else 
+            (evaluate rinfo (get-program-info (car resRol) plist) rlist plist matches))
+    )
+  )
+
+(define (offer rinfo rlist plist matches)
+(offer-helper (resRol rinfo) rinfo rlist plist matches)
+  )
+
+#|
+In order to implement the McVitie-Wilson algorithm, we will need to implement evaluate as well
+
+> (define M1 (offer (caddr RLIST) RLIST PLIST
+(offer (cadr RLIST) RLIST PLIST
+(offer (car RLIST) RLIST PLIST '()))))
+> (define M2 (evaluate (get-resident-info 226 RLIST)
+(get-program-info "HEP" PLIST) RLIST PLIST M1))
+> M2
+(("HEP" ((226 . 4) (403 . 0))) ("NRS" ((517 . 1) (574 . 0))))
+> (evaluate (get-resident-info 913 RLIST) (get-program-info "HEP"
+PLIST) RLIST PLIST M2)
+(("MMI" ((226 . 2))) ("HEP" ((913 . 2) (403 . 0)))
+("NRS" ((517 . 1) (574 . 0))))
+|#
+(define (in-matches? pid matches)
+  (cond ((null? matches) #f)
+        ((string=? pid (caar matches)) #t)
+        (else (in-matches? pid (cdr matches)))
+    )
+  )
+
+(define (update-match updated-entry matches)
+  (if (in-matches? (car updated-entry) matches)
+  (cond ((string=? (car updated-entry) (caar matches))
+                (cons updated-entry (cdr matches)))
+        (else (cons (car matches) (update-match updated-entry (cdr matches))))
+   )
+  (cons updated-entry matches)
+  )
+  )
+
+(define (least-preferred L) (caadr L))
+
+(define (evaluate rinfo pinfo rlist plist matches)
+  (cond ((= (rank (resID rinfo) pinfo) -1) matches)
+
+    ((null? (cdr (get-match (programID pinfo) matches)))
+              (update-match (add-resident-to-match (cons (resID rinfo) (rank (resID rinfo) pinfo)) (get-match (programID pinfo) matches)) matches)
+              )
+    
+    ((> (get-quota pinfo) (length (cadr (get-match (programID pinfo) matches))))
+           (update-match (add-resident-to-match (cons (resID rinfo) (rank (resID rinfo) pinfo)) (get-match (programID pinfo) matches)) matches))
+    
+    ((> (cdr (least-preferred (get-match (programID pinfo) matches))) (rank (resID rinfo) pinfo))
+            (let* ((r-prime (car (least-preferred (get-match(programID pinfo) matches))))
+                   (entry-without-r-prime (list (programID pinfo) (cdadr (get-match (programID pinfo) matches))))
+                   (entry-with-r (add-resident-to-match (cons (resID rinfo) (rank (resID rinfo) pinfo)) entry-without-r-prime))
+                   (updated-matches (update-match entry-with-r matches)))
+              (offer (get-resident-info r-prime rlist) rlist plist updated-matches)))
+    (else matches)
+            )
+    )
+  
